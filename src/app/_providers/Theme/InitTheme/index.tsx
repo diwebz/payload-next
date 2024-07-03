9c0266a529aebd1d902a95d8b6fcbf77149fdd0a
d_nesh <dine11649@gmail.com>
2024-07-04 00:18:15 +0900

    Create checkout, order-confirm, user account, oder history pages with necessary components

--- src/app/_providers/Theme/InitTheme/index.tsx
+++ src/app/_providers/Theme/InitTheme/index.tsx
@@ -7,7 +7,8 @@ export const InitTheme: React.FC = () => {
     // eslint-disable-next-line @next/next/no-before-interactive-script-outside-document
     <Script
       id="theme-script"
-      strategy="beforeInteractive"
+      // below line commented out by me, because of a Next js bug
+      // strategy="beforeInteractive"
       dangerouslySetInnerHTML={{
         __html: `
   (function () {
